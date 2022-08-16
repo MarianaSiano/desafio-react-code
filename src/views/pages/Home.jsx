8cd08064f1170a171cb7534eb65c5889c34ea3ab
Mariana <maryhsiano@hotmail.com>
2022-08-16 13:49:36 -0300

    Adicionando algumas telas

---
+++ src/views/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = props => (
+    <div className="Home">
+        <h1>Seja Bem-Vindo!!</h1>
+    </div>
+)
+
+export default Home
---
+++ src/views/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = props => (
+    <div className="NotFound">
+        <h1>Error 404</h1>
+        <h3>Ops... Página não encotrada!! Verifique se digitou o endereço correto</h3>
+    </div>
+)
+
+export default NotFound
