70213bf11f0d6f9f301bff74ab812034dc13ee6e
Mariana <maryhsiano@hotmail.com>
2022-08-16 10:54:23 -0300

    Estruturando o projeto

---
+++ src/views/App.jsx
@@ -0,0 +1,19 @@
+//Importes
+//CSS
+import './App.css';
+
+//React
+import React from 'react';
+
+//Router
+import { BrowserRouter as Router } from 'react-router-dom';
+
+//Components
+
+const App = props => (
+  <div className='App'>
+    <Router></Router>
+  </div>
+)
+
+export default App;
