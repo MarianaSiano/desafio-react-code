7991f42a3e8baa7aecf43747c5909192b3c7d8e6
Mariana <maryhsiano@hotmail.com>
2022-08-25 11:29:10 -0300

    Adicionando a imagem da logo, cor do footer e da navbar

--- src/views/pages/Login.jsx
+++ src/views/pages/Login.jsx
@@ -5,20 +5,35 @@ import { useState } from 'react';
 import PageTitle from '../../components/layouts/PageTitle';
 
 const Login = props => {
-    const [name, setName] = useState("")
-    const [senha, setSenha] = useState("")
+    const [name, setName] = useState("");
+    const [senha, setSenha] = useState("");
+
+    function senhaCorreta(senha) {
+        if(senha === this.senha) {
+            //Entrar na dashboard
+        }
+
+        else {
+            //Retornar senha incorreta
+        }
+    }
 
     return (
         <div className="Login">
             <PageTitle title="Login" subtitle="Faça seu login" />
 
-            <div className='email'>
-                <input type="text" className="input" placeholder="e-mail" value={name} onChange={e => setName(e.target.value)} />
-            </div>
+            <form className='formLogin'>
+                <div className='email'>
+                    <input type="text" className="input" placeholder="e-mail" value={name} onChange={e => setName(e.target.value)} />
+                </div>
 
-            <div className='senha'>
-                <input type="password" className="input" placeholder='senha' value={senha} onChange={e => setSenha(e.target.value)} />
-            </div>
+                <div className='senha'>
+                    <input type="password" className="input" placeholder='senha' value={senha} onChange={e => setSenha(e.target.value)} />
+                </div>
+
+                <button className="btn" type="submit">Entrar</button>
+            </form>
+
         </div>
     )
 }
