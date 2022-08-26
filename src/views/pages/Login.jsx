1d3d63cf85f3d0a2258cebceaad38bb3f0f989bd
Mariana <maryhsiano@hotmail.com>
2022-08-26 20:17:47 -0300

    Login praticamente pronto

--- src/views/pages/Login.jsx
+++ src/views/pages/Login.jsx
@@ -1,6 +1,7 @@
 import './css/Login.css';
 
 import React from "react";
+import { Link } from 'react-router-dom';
 import PageTitle from '../../components/layouts/PageTitle';
 
 const Login = props => {
@@ -8,7 +9,7 @@ const Login = props => {
         <div className="Login">
             <PageTitle title="Login" subtitle="É membro? Faça seu login!" />
 
-            <form className='formLogin'>
+            <form method='' action='/login'>
                 <div className='email'>
                 <label htmlFor="password" id='password'><p><strong>Login (E-mail cadastrado)</strong></p></label>
                     <input type="text" className="input" placeholder="E-mail" />
@@ -19,11 +20,16 @@ const Login = props => {
                     <input type="password" className="input" placeholder='Senha' />
                 </div>
 
-                <div>
-                    <label htmlFor="rememberMe">Remember me</label>
-                </div>
-
                 <button className="btn" type="submit">Entrar</button>
+                
+                {/*<div id='rememberMe'>
+                    <input type='checkbox' value='' />
+                    <label htmlFor="rememberMe">Remember me</label>
+                </div>*/}
+
+                <div id='forgotPassword'>
+                    <span><Link to='#'>Esqueceu a senha?</Link></span>
+                </div>
             </form>
 
         </div>
