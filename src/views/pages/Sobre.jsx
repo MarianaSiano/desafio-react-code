4e74005c9c44913f332fdec14e75f05e4b383955
Mariana <maryhsiano@hotmail.com>
2022-08-26 23:33:30 -0300

    index.html atualizada

--- src/views/pages/Sobre.jsx
+++ src/views/pages/Sobre.jsx
@@ -4,9 +4,11 @@ import React from "react";
 const Sobre = props => {
     return (
         <div className="Sobre">
-            <h3>Quem Somos</h3>
-            <p>Nossa Empresa Júnior é uma empresa de consultoria na área de transportes, atuando desde 1992.</p>
-            <p>Trabalhamos com consultoria em diversos transportes comerciais, como aviões bimotores, trens e caminhões.</p>
+            <div className="quem-somos">
+                <h3>Quem Somos</h3>
+                <p>Nossa Empresa Júnior é uma empresa de consultoria na área de transportes, atuando desde 1992.</p>
+                <p>Trabalhamos com consultoria em diversos transportes comerciais, como aviões bimotores, trens e caminhões.</p>
+            </div>
         </div>
     )
 }
