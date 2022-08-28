9f3f7466c98b62aaa6b9ecce258d9019118fe5b6
Mariana <maryhsiano@hotmail.com>
2022-08-28 16:06:36 -0300

    useState e useEffect dos membros feito

--- src/views/pages/Membros.jsx
+++ src/views/pages/Membros.jsx
@@ -2,14 +2,26 @@
 import React, { useState, useEffect } from "react";
 
 //Card
-import Card from '../../components/layouts/Card'
+import Card from '../../components/layouts/Card';
+
 //CSS
 import './css/Membros.css';
 
 const Membros = props => {
     function Membros() {
         const [membros, setMembros] = useState([])
+        const getMembros = async(url) => {
+            const resposta = await fetch(url);
+            const data = await resposta.json();
+
+            setMembros(data);
+        }
     }
+
+    useEffect(() => {
+        const membrosUrl = "http://localhost:9000/membros";
+    }, [])
+
     return (
         <div className="Membros"></div>
     )
