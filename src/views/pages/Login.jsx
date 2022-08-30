54f0f53a62ee88d5a171e54a331f493ec06427cb
Mariana <maryhsiano@hotmail.com>
2022-08-30 14:13:14 -0300

    Login praticamente pronto

--- src/views/pages/Login.jsx
+++ src/views/pages/Login.jsx
@@ -9,23 +9,30 @@ const Login = props => {
         <div className="Login">
             <PageTitle title="Login" subtitle="É membro? Faça seu login!" />
 
-            <form method='' action='/login'>
+            <form method='' action='/login'>                
                 <div className='email'>
-                <label htmlFor="password" id='password'><p><strong>Login (E-mail cadastrado)</strong></p></label>
+                    <label htmlFor="password" id='password'><p>Login (E-mail cadastrado)</p></label>
                     <input type="text" className="input" placeholder="E-mail" />
                 </div>
 
                 <div className='senha'>
-                    <label htmlFor="password" id='password'><p><strong>Senha</strong></p></label>
+                    <label htmlFor="password" id='password'><p>Senha</p></label>
                     <input type="password" className="input" placeholder='Senha' />
                 </div>
 
-                <button className="btn" type="submit">Entrar</button>
+                <Link to='/'>
+                    <button className="btn" type="submit">Entrar</button>
+                </Link>
                 
                 {/*<div id='rememberMe'>
                     <input type='checkbox' value='' />
                     <label htmlFor="rememberMe">Remember me</label>
                 </div>*/}
+
+                
+                <div id='ehMembro'>
+                    <span>Já é membro? <Link to='#'>Cadastre-se</Link></span>
+                </div>
 
                 <div id='forgotPassword'>
                     <span><Link to='#'>Esqueceu a senha?</Link></span>
